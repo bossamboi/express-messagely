763dccd063609e238981b0d992bc64e24bb44782
Eric Sohng <ericsohng@gmail.com>
2022-04-18 12:23:53 -0700

    implement User class functions

--- models/user.js
+++ models/user.js
@@ -2,65 +2,173 @@
 
 /** User of the site. */
 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("../db");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
+const { NotFoundError } = require("../expressError");
+
 class User {
+	/** Register new user. Returns
+	 *    {username, password, first_name, last_name, phone}
+	 */
 
-  /** Register new user. Returns
-   *    {username, password, first_name, last_name, phone}
-   */
+	static async register({ username, password, first_name, last_name, phone }) {
+		const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 
-  static async register({ username, password, first_name, last_name, phone }) {
-  }
+		const result = await db.query(
+			`INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+        VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+        RETURNING username, password, first_name, last_name, phone`,
+			[username, hashedPassword, first_name, last_name, phone]
+		);
 
-  /** Authenticate: is username/password valid? Returns boolean. */
+		return result.rows[0];
+	}
 
-  static async authenticate(username, password) {
-  }
+	/** Authenticate: is username/password valid? Returns boolean. */
 
-  /** Update last_login_at for user */
+	static async authenticate(username, password) {
+		const result = await db.query(
+			"SELECT password FROM users WHERE username = $1",
+			[username]
+		);
+		const user = result.rows[0];
 
-  static async updateLoginTimestamp(username) {
-  }
+		if (user) {
+			return await bcrypt.compare(password, user.password);
+		}
+		return false;
+	}
 
-  /** All: basic info on all users:
-   * [{username, first_name, last_name}, ...] */
+	/** Update last_login_at for user */
 
-  static async all() {
-  }
+	static async updateLoginTimestamp(username) {
+		await db.query(
+			`UPDATE users SET last_login_at = current_timestamp
+        WHERE username = $1`,
+			[username]
+		);
+	}
 
-  /** Get: get user by username
-   *
-   * returns {username,
-   *          first_name,
-   *          last_name,
-   *          phone,
-   *          join_at,
-   *          last_login_at } */
+	/** All: basic info on all users:
+	 * [{username, first_name, last_name}, ...] */
 
-  static async get(username) {
-  }
+	static async all() {
+		const results = await db.query(
+			`SELECT username, first_name, last_name 
+        FROM users`
+		);
+		return results.rows;
+	}
 
-  /** Return messages from this user.
-   *
-   * [{id, to_user, body, sent_at, read_at}]
-   *
-   * where to_user is
-   *   {username, first_name, last_name, phone}
-   */
+	/** Get: get user by username
+	 *
+	 * returns {username,
+	 *          first_name,
+	 *          last_name,
+	 *          phone,
+	 *          join_at,
+	 *          last_login_at } */
 
-  static async messagesFrom(username) {
-  }
+	static async get(username) {
+		const result = await db.query(
+			`SELECT username, 
+              first_name, 
+              last_name,
+              phone,
+              join_at,
+              last_login_at 
+        FROM users
+        WHERE username = $1`,
+			[username]
+		);
 
-  /** Return messages to this user.
-   *
-   * [{id, from_user, body, sent_at, read_at}]
-   *
-   * where from_user is
-   *   {id, first_name, last_name, phone}
-   */
+		if (result.rows[0] === undefined) {
+			throw new NotFoundError();
+		} else {
+			return result.rows[0];
+		}
+	}
 
-  static async messagesTo(username) {
-  }
+	/** Return messages from this user.
+	 *
+	 * [{id, to_user, body, sent_at, read_at}]
+	 *
+	 * where to_user is
+	 *   {username, first_name, last_name, phone}
+	 */
+
+	static async messagesFrom(username) {
+		const results = await db.query(
+			`SELECT m.id,
+		          m.to_username,
+		          t.first_name AS to_first_name,
+		          t.last_name AS to_last_name,
+		          t.phone AS to_phone,
+		          m.body,
+		          m.sent_at,
+		          m.read_at
+		    FROM messages AS m
+		            JOIN users AS t ON m.to_username = t.username
+                JOIN users as f ON m.from_username = f.username
+		    WHERE f.username = $1`,
+			[username]
+		);
+
+		// result.rows is an array of objects with the SELECTED columns
+		return results.rows.map((m) => ({
+			id: m.id,
+			to_user: {
+				username: m.to_username,
+				first_name: m.to_first_name,
+				last_name: m.to_last_name,
+				phone: m.to_phone,
+			},
+			body: m.body,
+			sent_at: m.sent_at,
+			read_at: m.read_at,
+		}));
+	}
+
+	/** Return messages to this user.
+	 *
+	 * [{id, from_user, body, sent_at, read_at}]
+	 *
+	 * where from_user is
+	 *   {username, first_name, last_name, phone}
+	 */
+
+	static async messagesTo(username) {
+		const results = await db.query(
+			`SELECT m.id,
+		          m.from_username,
+		          f.first_name AS from_first_name,
+		          f.last_name AS from_last_name,
+		          f.phone AS from_phone,
+		          m.body,
+		          m.sent_at,
+		          m.read_at
+		    FROM messages AS m
+		            JOIN users AS t ON m.to_username = t.username
+                JOIN users as f ON m.from_username = f.username
+		    WHERE t.username = $1`,
+			[username]
+		);
+
+		return results.rows.map((m) => ({
+			id: m.id,
+			from_user: {
+				username: m.from_username,
+				first_name: m.from_first_name,
+				last_name: m.from_last_name,
+				phone: m.from_phone,
+			},
+			body: m.body,
+			sent_at: m.sent_at,
+			read_at: m.read_at,
+		}));
+	}
 }
-
 
 module.exports = User;
